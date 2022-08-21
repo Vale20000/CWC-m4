9f0479f87ee265fa725012644b4b4636a53d2784
Valentino Masetti <valentinomasetti@hotmail.it>
2022-08-21 12:29:18 +0200

    - build UI in the RecipeFeaturedView

--- RecipeListApp/Views/RecipeFeaturedView.swift
+++ RecipeListApp/Views/RecipeFeaturedView.swift
@@ -18,12 +18,64 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading) {
+            
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+            
+            GeometryReader { geo in
+                TabView {
+                    
+                    // Loop through each recipe
+                    ForEach (0..<model.recipes.count) { index in
+                        
+                        // Only show those that should be featured
+                        if model.recipes[index].featured {
+                            
+                            // Recipe card
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(Color.white)
+                                
+                                VStack(spacing: 0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
+                                }
+                            }.frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                                .cornerRadius(15)
+                                .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5) // -5 alla x e +5 alla y dell'ombra
+                        }
+                        
+                    }
+                    
+                    // Serve a impostare la tab view come swipable e con .automatic dico che voglio visualizzare i puntini dell'indice solo quando c'è più di una ricetta
+                }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                    .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always)) // Now dots have a background
+            }
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearthy")
+            }.padding([.leading, .bottom])
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
